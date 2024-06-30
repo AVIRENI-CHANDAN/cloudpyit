de031116585664f4b76c41949081e63525a24e6e
AVIRENI CHANDAN <avireni.chandanchandu43657@gmail.com>
2024-06-30 09:48:28 +0530

    Courses page added

--- cloudpyit/src/components/CoursesPage/CoursesPage.js
+++ cloudpyit/src/components/CoursesPage/CoursesPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import students_logo from '../../images/user.png';
+import likes_logo from '../../images/heart.png';
 import styles from './CoursesPage.module.scss';
 
 class CoursesPage extends React.Component {
@@ -6,7 +8,25 @@ class CoursesPage extends React.Component {
     super(props);
     this.page_title = "Courses";
     this.page_description = "This structured approach to software training and project support, you can ensure that participants gain the necessary skills to develop and manage Applications effectively. This approach also ensures that projects are completed successfully, meeting the client's needs and goals.";
+    this.state = {
+      courses_data: {}
+    };
   }
+
+  componentDidMount() {
+    fetch("/courses/popular")
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok');
+      })
+      .then(data => {
+        this.setState({ courses_data: data });
+      })
+      .catch(error => console.error("ERROR in fetching" + error.message));
+  }
+
   render() {
     return (
       <div className={styles.CoursesPage}>
@@ -23,7 +43,55 @@ class CoursesPage extends React.Component {
           </div>
         </div>
         <div className={styles.PageBody}>
-          PageBody
+          <div className={styles.PageBox}>
+            <div className={styles.CoursesList}>
+              {Object.entries(this.state.courses_data).map(([key, value]) => (
+                <div className={styles.CourseListItem} key={key}>
+                  <div className={styles.CourseImage}>
+                    <img src={`/${value.image}`} alt={value.title} />
+                  </div>
+                  <div className={styles.AboutCourse}>
+                    <div className={styles.CourseTitle}>
+                      <div className={styles.TitleWrapper}>
+                        {value.title}
+                      </div>
+                    </div>
+                    <div className={styles.CourseDescription}>
+                      <div className={styles.DescriptionWrapper}>
+                        {value.description}
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.TrainerBox}>
+                    <div className={styles.TrainerImage}>
+                      <img src={`${value.trainer.profile_pic}`} alt="trainer" />
+                    </div>
+                    <div className={styles.TrainerName}>
+                      {value.trainer.name}
+                    </div>
+                    <div className={styles.CourseStats}>
+                      <div className={styles.Stats}>
+                        <div className={styles.Logo}>
+                          <img src={students_logo} alt='Students' />
+                        </div>
+                        <div className={styles.Count}>
+                          {value.students}
+                        </div>
+                      </div>
+                      <div className={styles.Stats}>
+                        <div className={styles.Logo}>
+                          <img src={likes_logo} alt='Likes' />
+                        </div>
+                        <div className={styles.Count}>
+                          {value.likes}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     );
---
+++ tempdb.py
@@ -0,0 +1,113 @@
+popular_courses = {
+    0: {
+        "title": "AWS DevOps",
+        "description": "AWS DevOps integrates cultural philosophies, practices, and tools to enhance an organization's ability to deliver applications and services at high velocity using Amazon Web Services. By leveraging AWS services like CodePipeline, CloudFormation, and CloudWatch, AWS DevOps facilitates continuous integration and continuous deployment (CI/CD), infrastructure as code (IaC), monitoring, and automation. This approach enhances speed, reliability, scalability, security, and collaboration, enabling teams to innovate faster and manage infrastructure more efficiently. AWS DevOps empowers organizations to streamline their development and operations processes, ensuring rapid delivery and high-quality software.",
+        "cost": "₹2500",
+        "trainer": {
+            "name": "Trainer devops",
+            "profile_pic": "images/users/profile_pic_default.png",
+        },
+        "students": 50,
+        "likes": 67,
+        "image": "images/courses/AWS-DevOps.jpg",
+    },
+    1: {
+        "title": "Azure Admin",
+        "description": "An Azure Administrator is responsible for implementing, monitoring, and maintaining Microsoft Azure solutions, including major services related to computing, storage, network, and security. This role involves managing Azure subscriptions and resources, implementing and managing storage, deploying and managing virtual machines, configuring and managing virtual networks, and monitoring and backing up Azure resources. Azure Administrators ensure that the Azure environment runs smoothly and securely, troubleshoot any issues that arise, optimize performance, and manage identity and access through Azure Active Directory. They play a crucial role in enabling organizations to leverage the full potential of Azure's cloud platform for their operations.",
+        "cost": "₹5500",
+        "trainer": {
+            "name": "Trainer azure",
+            "profile_pic": "images/users/profile_pic_default.png",
+        },
+        "students": 45,
+        "likes": 78,
+        "image": "images/courses/Azure-admin.jpg",
+    },
+    2: {
+        "title": "Python Full Stack Developer",
+        "description": "A Python Full Stack Developer is proficient in both front-end and back-end development, utilizing Python for server-side programming while mastering front-end technologies like HTML, CSS, and JavaScript. They are skilled in frameworks such as Django or Flask for back-end development, ensuring robust server logic, database interactions, and API integrations. On the front-end, they employ libraries and frameworks like React, Angular, or Vue.js to create dynamic, responsive user interfaces. Python Full Stack Developers are adept at working with databases, version control systems like Git, and deployment tools, enabling them to build, test, deploy, and maintain complete web applications from end to end, ensuring a seamless and efficient user experience.",
+        "cost": "₹11200",
+        "trainer": {
+            "name": "Trainer python full stack",
+            "profile_pic": "images/users/profile_pic_default.png",
+        },
+        "students": 56,
+        "likes": 87,
+        "image": "images/courses/PythonFullStackPackage.jpg",
+    },
+    3: {
+        "title": "Data Scientist",
+        "description": "A Data Scientist utilizes statistical analysis, machine learning, and data visualization techniques to extract insights and knowledge from structured and unstructured data. They work with large datasets, using tools such as Python, R, and SQL, and frameworks like TensorFlow and PyTorch to develop predictive models and algorithms. Data Scientists communicate their findings through reports and dashboards, helping organizations make data-driven decisions to optimize operations and strategy.",
+        "cost": "₹13500",
+        "trainer": {
+            "name": "Trainer data science",
+            "profile_pic": "images/users/profile_pic_default.png",
+        },
+        "students": 62,
+        "likes": 90,
+        "image": "images/courses/Data Science.jpg",
+    },
+    4: {
+        "title": "Cybersecurity Specialist",
+        "description": "A Cybersecurity Specialist is responsible for protecting an organization’s computer systems and networks from cyber threats. They implement security measures, monitor systems for vulnerabilities, and respond to security incidents. This role involves staying updated with the latest security trends, performing risk assessments, and ensuring compliance with security policies and regulations. They use tools and frameworks like SIEM, firewalls, and intrusion detection systems to safeguard sensitive information.",
+        "cost": "₹12000",
+        "trainer": {
+            "name": "Trainer cybersecurity",
+            "profile_pic": "images/users/profile_pic_default.png",
+        },
+        "students": 40,
+        "likes": 73,
+        "image": "images/courses/PythonFullStackPackage.jpg",
+    },
+    5: {
+        "title": "Machine Learning Engineer",
+        "description": "A Machine Learning Engineer designs, builds, and deploys machine learning models and systems. They work with large datasets, leveraging algorithms and tools like TensorFlow, Keras, and scikit-learn to create models that can make predictions or automate decision-making processes. This role involves data preprocessing, model training and evaluation, and deploying models to production environments. Machine Learning Engineers collaborate with data scientists and software engineers to integrate models into applications and services.",
+        "cost": "₹15000",
+        "trainer": {
+            "name": "Trainer ml engineer",
+            "profile_pic": "images/users/profile_pic_default.png",
+        },
+        "students": 35,
+        "likes": 85,
+        "image": "images/courses/PythonFullStackPackage.jpg",
+    },
+    6: {
+        "title": "DevOps Engineer",
+        "description": "A DevOps Engineer bridges the gap between software development and IT operations, focusing on automating and streamlining the development, testing, and deployment processes. They use tools like Jenkins, Docker, Kubernetes, and Ansible to create CI/CD pipelines, manage infrastructure as code, and ensure system reliability and scalability. DevOps Engineers work closely with development teams to improve collaboration, efficiency, and delivery speed while maintaining a robust and secure infrastructure.",
+        "cost": "₹8000",
+        "trainer": {
+            "name": "Trainer devops engineer",
+            "profile_pic": "images/users/profile_pic_default.png",
+        },
+        "students": 48,
+        "likes": 92,
+        "image": "images/courses/PythonFullStackPackage.jpg",
+    },
+    7: {
+        "title": "Cloud Solutions Architect",
+        "description": "A Cloud Solutions Architect is responsible for designing and implementing cloud-based solutions that meet an organization’s business needs. They develop architecture blueprints, select appropriate cloud services, and ensure scalability, security, and cost-efficiency. This role requires deep knowledge of cloud platforms like AWS, Azure, or Google Cloud, as well as an understanding of networking, databases, and application development. Cloud Solutions Architects work closely with stakeholders to align cloud strategies with business goals.",
+        "cost": "₹14000",
+        "trainer": {
+            "name": "Trainer cloud architect",
+            "profile_pic": "images/users/profile_pic_default.png",
+        },
+        "students": 30,
+        "likes": 88,
+        "image": "images/courses/PythonFullStackPackage.jpg",
+    },
+}
+
+available_courses = [
+    "AWS DEVOPS",
+    "AZURE ADMIN",
+    "PYTHON FULL STACK DEVELOPER",
+    "DATA SCIENCE",
+    "AIML",
+    "C PROGRAMMING",
+    "C++ PROGRAMMING",
+    "Treasury Analyst",
+    "SAP ABAP",
+    "POWER BI",
+    "AZURE DEVOPS",
+    "AWS ADMIN",
+]
