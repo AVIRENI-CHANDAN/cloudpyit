320bb8d5149c9e6522435196b9d85b0c09302415
AVIRENI CHANDAN <avireni.chandanchandu43657@gmail.com>
2024-07-05 09:26:37 +0530

    Code formatting for MapComponent

--- cloudpyit/src/components/ContactPage/MapComponent.js
+++ cloudpyit/src/components/ContactPage/MapComponent.js
@@ -2,17 +2,10 @@
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import React from 'react';
 
-const MapComponent = () => {
-    const mapStyles = {
-        height: "400px",
-        width: "100%"
-    };
+function MapComponent() {
+    const mapStyles = { height: "400px", width: "100%" };
 
-    const defaultCenter = {
-        lat: 17.451629, lng: 78.381147
-        // lat: 40.748817,
-        // lng: -73.985428
-    };
+    const defaultCenter = { lat: 17.451629, lng: 78.381147 };
 
     return (
         <LoadScript googleMapsApiKey="AIzaSyB8pgDs2AHwCuLf9lXaozsg79yL2FNuG4U">
@@ -21,6 +14,6 @@ const MapComponent = () => {
             </GoogleMap>
         </LoadScript>
     );
-};
+}
 
 export default MapComponent;
