382971f0865851356c25cf20cceda236bfd6db06
AVIRENI CHANDAN <avireni.chandanchandu43657@gmail.com>
2024-07-05 13:49:30 +0530

    loading element for map component added

--- cloudpyit/src/components/ContactPage/MapComponent.js
+++ cloudpyit/src/components/ContactPage/MapComponent.js
@@ -7,7 +7,7 @@ function MapComponent() {
     const defaultCenter = { lat: 17.452042, lng: 78.390146 };
 
     return (
-        <LoadScript googleMapsApiKey="AIzaSyB8pgDs2AHwCuLf9lXaozsg79yL2FNuG4U">
+        <LoadScript googleMapsApiKey="AIzaSyB8pgDs2AHwCuLf9lXaozsg79yL2FNuG4U" loadingElement={<div>Loading...</div>}>
             <GoogleMap mapContainerStyle={mapStyles} zoom={16} center={defaultCenter}>
                 <Marker position={defaultCenter} />
             </GoogleMap>
