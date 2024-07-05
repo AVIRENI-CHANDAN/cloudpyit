5c6041b6661402c57e770176061fb0d4b197f12f
AVIRENI CHANDAN <avireni.chandanchandu43657@gmail.com>
2024-07-05 09:34:51 +0530

    Marker component updated with zoom and lat, lng.

--- cloudpyit/src/components/ContactPage/MapComponent.js
+++ cloudpyit/src/components/ContactPage/MapComponent.js
@@ -1,15 +1,14 @@
-// MapComponent.js
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import React from 'react';
 
 function MapComponent() {
     const mapStyles = { height: "400px", width: "100%" };
 
-    const defaultCenter = { lat: 17.451629, lng: 78.381147 };
+    const defaultCenter = { lat: 17.452042, lng: 78.390146 };
 
     return (
         <LoadScript googleMapsApiKey="AIzaSyB8pgDs2AHwCuLf9lXaozsg79yL2FNuG4U">
-            <GoogleMap mapContainerStyle={mapStyles} zoom={13} center={defaultCenter}>
+            <GoogleMap mapContainerStyle={mapStyles} zoom={16} center={defaultCenter}>
                 <Marker position={defaultCenter} />
             </GoogleMap>
         </LoadScript>
